223adc81d62d821e1e0e7613b7efa8405c0d70ba
emilekhairallahcnam <emile.khairallah@isae.edu.lb>
2023-12-17 19:31:02 +0200

    tp done

--- tp2/question3/IHMFahrenheit.java
+++ tp2/question3/IHMFahrenheit.java
@@ -25,25 +25,31 @@ public class IHMFahrenheit extends JFrame implements ActionListener{
     boutonDeConversion.addActionListener( this );
   }
 
-  /** 
-   * méthode déclenchée lorsque le bouton de conversion est appuyé. 
-   * remarquer que le champs de droite (les degrés Celsius) n'est pas éditable.
-   * @param ae l'événement transmis
-   */
-  public void actionPerformed( ActionEvent ae ){
-    try{
-      int fahrenheit = 0; // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
-      float celsius = 0F; // à compléter, en appelant la méthode ad'hoc de la question2 
-      // un test ici pour le zéro absolu (-273.1)
-
-      sortie.setText( Float.toString( celsius));
-    }catch(NumberFormatException nfe){
-      sortie.setText("error ! ");
+    public void actionPerformed( ActionEvent ae ){
+        try{
+            int fahrenheit = Integer.parseInt(entree.getText());
+            float celsius = fahrenheitEnCelsius(fahrenheit);
+            if(celsius < -273.1){
+                sortie.setText("-273.1");
+            }else{
+                sortie.setText(Float.toString(celsius));
+            }
+        }catch(NumberFormatException nfe){
+            sortie.setText("error !");
+        }
     }
-  }
+    
+    public float fahrenheitEnCelsius( int f){
+        float x = 5;
+        float y = 9;
+        float z = (x/y * (f - 32));
+        z = z * 10;
+        z = (float)Math.floor(z);
+        z = z / 10;
+        return z;
+    }  
   
-  
-  public static void main(String[] args){
-      new IHMFahrenheit();
+    public static void main(String[] args){
+        new IHMFahrenheit();
     }
 }
